249ce7e4c7dfbeeb9a9c6f59f2fcccda24876264
Lloyd Brown <lloyd_brown@byu.edu>
2015-12-01 10:19:56 -0700

    basic structure for urwid UI, but still need to fix several things

--- isshd_logparser.py
+++ isshd_logparser.py
@@ -12,6 +12,8 @@ import unicodedata
 
 import pprint
 
+import urwid
+import urwid.raw_display
 
 
 def openFile(filename=None):
@@ -33,6 +35,31 @@ def decodeData(data=None):
     else:
         return escape_control_characters(urllib.unquote_plus(data).lstrip())
 
+def urwidMain(eventslist, sessionid):
+    mainheader = urwid.AttrWrap(urwid.Text("isshd_logparser for session %s" % sessionid), 'header')
+    
+    
+    columns = urwid.Columns([
+        (28, urwid.Frame(urwid.LineBox(urwid.ListBox(eventslist['date']), title="Date/Time"))),
+        urwid.Frame(urwid.LineBox(urwid.ListBox(eventslist['client']), title="Client Typed")),
+        urwid.Frame(urwid.LineBox(urwid.ListBox(eventslist['server']), title="Server Said"))
+    ])
+        
+    
+    frame = urwid.Frame( columns, header=mainheader)
+
+    
+    def unhandled(key):
+        if key in ('q','Q'):
+            raise urwid.ExitMainLoop()
+
+    print "Debugging: right before MainLoop"
+
+    loop = urwid.MainLoop(frame, unhandled_input=unhandled)
+    loop.run()
+    
+    print "Debugging: right after MainLoop"
+
 if __name__=="__main__":
     argparser = argparse.ArgumentParser(description="Extract info from isshd logs, making it more intelligible")
     argparser.add_argument('-s', '--sessionid', action='store', metavar="SESSIONID", dest='sessionid', help="REQUIRED OPTION. Session to extract from the log.  Note that this is usually available in the Bro notification email, right before the username.", required=True)
@@ -52,10 +79,11 @@ if __name__=="__main__":
     
     session_regex_match_re = re.compile(session_regex_match)
     
-    sessionevents={}
+    columnobjs={ 'date':[], 'client': [], 'server': [] }
+    sessionevents = {}
     
     for filename in args.logfiles:
-        print "Parsing parse %s" % filename
+        print "Parsing file %s" % filename
         
         fh = openFile(filename)
     
@@ -66,6 +94,15 @@ if __name__=="__main__":
                 matchcaptures = matchresults.groups()
                 
                 sessionevents[datetime.fromtimestamp(float(matchcaptures[1]))]={'type': matchcaptures[0], 'software': matchcaptures[2], 'host': decodeData(matchcaptures[3]), 'data': decodeData(matchcaptures[4])}
+                
+                if matchcaptures[0]=="channel_data_server_3":
+                    columnobjs['server'].append(urwid.Text(decodeData(matchcaptures[4]), wrap="clip"))
+                    columnobjs['client'].append(urwid.Text(""))
+                    columnobjs['date'].append(urwid.Text("%s"%datetime.fromtimestamp(float(matchcaptures[1]))))
+                elif matchcaptures[0]=="channel_data_client_3":
+                    columnobjs['client'].append(urwid.Text(decodeData(matchcaptures[4]), wrap="clip"))
+                    columnobjs['server'].append(urwid.Text(""))
+                    columnobjs['date'].append(urwid.Text("%s"%datetime.fromtimestamp(float(matchcaptures[1]))))                    
             line = fh.readline()
         
         fh.close()
@@ -73,6 +110,11 @@ if __name__=="__main__":
         print "Done parsing %s" % filename
         
     
-    for eventdatetime in sorted(sessionevents):
-        print "{when} - Type: {type}, Host: {host}, Data: {data}".format(when=eventdatetime, type=sessionevents[eventdatetime]['type'], host=sessionevents[eventdatetime]['host'], data=sessionevents[eventdatetime]['data'])
+    #for eventdatetime in sorted(sessionevents):
+        #print "{when} - Type: {type}, Host: {host}, Data: {data}".format(when=eventdatetime, type=sessionevents[eventdatetime]['type'], host=sessionevents[eventdatetime]['host'], data=sessionevents[eventdatetime]['data'])
+    
+    #pprint.pprint(columnobjs)
+    #pprint.pprint(sessionevents)
+    urwidMain(columnobjs, args.sessionid)
+    
     
