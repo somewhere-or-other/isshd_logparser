46af57e50a71db651736663ad5c33a1878f1a2e6
Lloyd Brown <lloyd_brown@byu.edu>
2015-12-04 09:35:44 -0700

    better handling for URWID not installed

--- isshd_logparser.py
+++ isshd_logparser.py
@@ -10,28 +10,9 @@ from datetime import datetime
 import string
 import unicodedata
 
-import pprint
+#import pprint
 
-import urwid
-#import urwid.raw_display
-
-
-class ItemWidget (urwid.WidgetWrap):
-
-    def __init__ (self, id, date, message, msgtype):
-        self.id = id
-        self.item = [
-            ('fixed', 28, urwid.Padding(urwid.AttrWrap(urwid.Text('%s' % date), 'date', 'focus'))),
-            urwid.Text((msgtype, '%s' % message))
-        ]
-        w = urwid.Columns(self.item)
-        self.__super.__init__(w)
-
-    def selectable (self):
-        return True
-
-    def keypress(self, size, key):
-        return key
+interactivedefault=True #change to false if you want non-interactive to be default
 
 def openFile(filename=None):
     if filename==None:
@@ -53,69 +34,104 @@ def decodeData(data=None):
         return escape_control_characters(urllib.unquote_plus(data).lstrip())
         #return urllib.unquote_plus(data).lstrip()
 
-def urwidMain(eventslist, sessionid):
-  
-    #main color pallette:
-    #Syntax:
-    # ( NAME , ForegroundColor, BackgroundColor, I'm Not Sure )
-    palette = [
-        ('date','light gray', '', 'standout'),
-        ('focus','dark red', '', 'standout'),
-        ('client', 'dark green', '', 'standout'),
-        ('clientbg', '', 'dark green', 'standout'),
-        ('server', 'brown', '', 'standout'),
-        ('serverbg', '', 'brown', 'standout'),
-        ('header','light red', 'black'),
-        ]  
-  
-    headerstring = "isshd_logparser for session %s" % sessionid
-    client_key_string='Client Input'
-    server_key_string='Server Output'
-    key_key_string='Key:'
-    
-    mainheader = urwid.AttrWrap(urwid.Text(headerstring), 'header')
 
 
- 
-    legend = urwid.Columns([
-        urwid.Padding(urwid.Text(key_key_string), left=5),
-        (2, urwid.AttrWrap(urwid.Text('', align='right'), 'clientbg')),
-        urwid.AttrWrap(urwid.Text(client_key_string, align='left'), 'client'),
-        (2, urwid.AttrWrap(urwid.Text('', align='right'), 'serverbg')),
-        urwid.AttrWrap(urwid.Text(server_key_string, align='left'), 'server')
-        ])
-    
-    headercolumns = urwid.Columns([
-        mainheader,
-        (len(key_key_string)+len(client_key_string)+len(server_key_string)+15, legend)
-        ])
-    
-
-    
-    #frame = urwid.Frame( urwid.ListBox(eventslist), header=mainheader)
-    frame = urwid.Frame( urwid.ListBox(eventslist), header=headercolumns)
-    
-    def unhandled(key):
-        if key in ('q','Q'):
-            raise urwid.ExitMainLoop()
-
-    loop = urwid.MainLoop(frame, palette, unhandled_input=unhandled)
-    loop.run()
 
 if __name__=="__main__":
     argparser = argparse.ArgumentParser(description="Extract info from isshd logs, making it more intelligible")
     argparser.add_argument('-s', '--sessionid', action='store', metavar="SESSIONID", dest='sessionid', help="REQUIRED OPTION. Session to extract from the log.  Note that this is usually available in the Bro notification email, right before the username.", required=True)
     argparser.add_argument('-l', '--logfile', action='append', metavar='LOGFILEPATH', dest='logfiles', help="Specifies the path or paths to extract data from.  May be specified multiple times, and log files may be compressed with '.gz' extension.", default=[])
-    argparser.add_argument('-i', '--interactive', action="store_true", dest="interactive", help="use colorized, interactive output (q to exit)", default=True)
+    argparser.add_argument('-i', '--interactive', action="store_true", dest="interactive", help="use colorized, interactive output (q to exit)", default=interactivedefault)
     argparser.add_argument('-n', '--noninteractive', action="store_false", dest="interactive", help="plain, non-interactive output")
     
     args = argparser.parse_args()
     
+    interactive_output = args.interactive
+    
+    try:
+        import urwid
+        
+        #we succeeded in importing.  Just use whatever the user already specified, which we've already put into "interactive_output"
+        #and define a bunch of stuff here, so if we don't have it, these won't throw errors about unknown names
+        
+        class ItemWidget (urwid.WidgetWrap):
+
+            def __init__ (self, id, date, message, msgtype):
+                self.id = id
+                self.item = [
+                    ('fixed', 28, urwid.Padding(urwid.AttrWrap(urwid.Text('%s' % date), 'date', 'focus'))),
+                    urwid.Text((msgtype, '%s' % message))
+                ]
+                w = urwid.Columns(self.item)
+                self.__super.__init__(w)
+
+            def selectable (self):
+                return True
+
+            def keypress(self, size, key):
+                return key
+
+        def urwidMain(eventslist, sessionid):
+        
+            #main color pallette:
+            #Syntax:
+            # ( NAME , ForegroundColor, BackgroundColor, I'm Not Sure )
+            palette = [
+                ('date','light gray', '', 'standout'),
+                ('focus','dark red', '', 'standout'),
+                ('client', 'dark green', '', 'standout'),
+                ('clientbg', '', 'dark green', 'standout'),
+                ('server', 'brown', '', 'standout'),
+                ('serverbg', '', 'brown', 'standout'),
+                ('header','light red', 'black'),
+                ]  
+        
+            headerstring = "isshd_logparser for session %s" % sessionid
+            client_key_string='Client Input'
+            server_key_string='Server Output'
+            key_key_string='Key:'
+            
+            mainheader = urwid.AttrWrap(urwid.Text(headerstring), 'header')
+
+
+        
+            legend = urwid.Columns([
+                urwid.Padding(urwid.Text(key_key_string), left=5),
+                (2, urwid.AttrWrap(urwid.Text('', align='right'), 'clientbg')),
+                urwid.AttrWrap(urwid.Text(client_key_string, align='left'), 'client'),
+                (2, urwid.AttrWrap(urwid.Text('', align='right'), 'serverbg')),
+                urwid.AttrWrap(urwid.Text(server_key_string, align='left'), 'server')
+                ])
+            
+            headercolumns = urwid.Columns([
+                mainheader,
+                (len(key_key_string)+len(client_key_string)+len(server_key_string)+15, legend)
+                ])
+            
+
+            
+            frame = urwid.Frame( urwid.ListBox(eventslist), header=headercolumns)
+            
+            def unhandled(key):
+                if key in ('q','Q'):
+                    raise urwid.ExitMainLoop()
+
+            loop = urwid.MainLoop(frame, palette, unhandled_input=unhandled)
+            loop.run()
+            
+        
+        
+        
+        
+        
+    except ImportError:
+        if (args.interactive):
+            print "Error: interactive output requires the URWID library (urwid.org), but I can't seem to find it.  Reverting to non-interactive output"
+        interactive_output=False
+    
     
     if args.logfiles==[]:
         args.logfiles.append("/usr/local/bro/logs/isshd/isshd.log");
-    
-    
     
 
     session_regex_match = r"^(channel_data_server_3|channel_data_client_3)\s+time=([.\d]+)\s+uristring=(\S+)\s+uristring=-?\d+%%3A(\S+)\s+count=%s\s+count=0\s+uristring=(\S+)\s*$" % args.sessionid
@@ -133,8 +149,8 @@ if __name__=="__main__":
     
         line = fh.readline()
         
-        if (args.interactive):
-            
+        if (interactive_output):
+            #TODO: handle sorting events by date/time, when sessions are split across multiple files
             while line:
                 matchresults = session_regex_match_re.search(line)
                 if matchresults != None:
@@ -148,7 +164,8 @@ if __name__=="__main__":
                         displaylines.append(ItemWidget(idcount, datetime.fromtimestamp(float(matchcaptures[1])), decodeData(matchcaptures[4]), 'client'))
                         idcount += 1
                 line = fh.readline()
-        else:
+                
+        else: #non-interactive output
             
             while line:
                 matchresults = session_regex_match_re.search(line)
@@ -173,7 +190,7 @@ if __name__=="__main__":
         
     
     
-    if (args.interactive):
+    if (interactive_output):
         urwidMain(displaylines, args.sessionid)
     else:
         for eventdatetime in sorted(sessionevents):
